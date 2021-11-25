dc1338521cd60649c162c8d7addc54891ac36625
DS Hwang <hds2g22@gmail.com>
2021-11-25 13:05:48 +0900

    Redux-React: Connecting the Store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
+ReactDOM.render(
+  <provider store={store}>
+  <App />
+  </provider>,
+  document.getElementById("root")
+ );
