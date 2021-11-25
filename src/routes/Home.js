78ba8ac87716e1052e3b07b5d8e2f344f6643b49
DS Hwang <hds2g22@gmail.com>
2021-11-25 18:57:43 +0900

    Deleting To Do

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import ToDo from "../components/ToDo";
 
-function Home({ toDos, addToDo }) {
-  const [text, setText] = useState("");
-  function onChange(e) {
-    setText(e.target.value);
-  }
-  function onSubmit(e) {
-    e.preventDefault();
-    addToDo(text);
-    setText("");
-  }
-  return (
-    <>
-      <h1>To Do</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" value={text} onChange={onChange} />
-        <button>Add</button>
-      </form>
-      <ul>{JSON.stringify(toDos)}</ul>
-    </>
-  );
+function Home(props) {
+    console.log(props)
+    let {toDos, addToDo} = props;
+    const [text, setText] = useState("");
+    function onChange(e) {
+        setText(e.target.value);
+    }
+    function onSubmit(e) {
+        e.preventDefault();
+        addToDo(text);
+        setText("");
+    }
+    return (
+        <>
+            <h1>To Do</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text" value={text} onChange={onChange} />
+                <button>Add</button>
+            </form>
+            <ul>
+                {toDos.map(toDo => (
+                <ToDo {...toDo} key={toDo.id} />
+                ))}
+           </ul>
+        </>
+    );
 }
 
 function mapStateToProps(state) {
